b7a4caedf61c90d92cfbc181ab133932e63c6dc8
leoJ2eePHP <leandro.j2ee@gmail.com>
2022-04-15 15:04:22 -0400

    Producción!

--- config/db.php
+++ config/db.php
@@ -3,20 +3,18 @@
 return [
     'class' => 'yii\db\Connection',
     // DESARROLLO !!
+    /*
     'dsn' => 'mysql:host=otzi.cl;dbname=cot27290_SAMQA',
     'username' => 'cot27290_SAMQA',
     'password' => '?)ZkEI(O]nDl',
     'charset' => 'utf8',
-
+    */
     //PRODUCCIÓN !!
-    /*
     'dsn' => 'mysql:host=127.0.0.1;dbname=cot27290_SAM',
     'emulatePrepare' => true,
     'username' => 'cot27290_SAM',
     'password' => '?)ZkEI(O]nDl',
     'charset' => 'utf8',
-    */
-    //'enableParamLogging' => true,
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
