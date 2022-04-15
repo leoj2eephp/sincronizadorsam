5208bedb8cf66a031dad6a0f58abfc0c0b750de9
leoJ2eePHP <leandro.j2ee@gmail.com>
2022-04-15 15:20:21 -0400

    fix

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-//$config = require __DIR__ . '/../config/web.php';
-$config = require __DIR__ . '/../config/web_local.php';
+$config = require __DIR__ . '/../config/web.php';
+//$config = require __DIR__ . '/../config/web_local.php';
 
 (new yii\web\Application($config))->run();
