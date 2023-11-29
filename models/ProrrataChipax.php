77e734a2c5053c16ad5b0f36d2af455c9d216871
leoj2eephp <leandro.j2ee@gmail.com>
2023-11-29 11:13:54 -0300

    Update. ProrrataChipax. Se aumenta el tamaño de linea_negocio y modelo.

--- models/ProrrataChipax.php
+++ models/ProrrataChipax.php
@@ -44,7 +44,7 @@ class ProrrataChipax extends \yii\db\ActiveRecord {
             [['id', 'linea_negocio', 'modelo', 'monto', 'periodo'], 'required'],
             [['id', 'cuenta_id', 'filtro_id', 'monto', 'compra_chipax_id', 'gasto_chipax_id', 'honorario_chipax_id', 'remuneracion_chipax_id'], 'integer'],
             [['periodo', 'monto_sumado', 'neto_impuesto'], 'safe'],
-            [['linea_negocio', 'modelo'], 'string', 'max' => 45],
+            [['linea_negocio', 'modelo'], 'string', 'max' => 100],
             [['id'], 'unique'],
             [['compra_chipax_id'], 'exist', 'skipOnError' => true, 'targetClass' => CompraChipax::class, 'targetAttribute' => ['compra_chipax_id' => 'id']],
             [['gasto_chipax_id'], 'exist', 'skipOnError' => true, 'targetClass' => GastoChipax::class, 'targetAttribute' => ['gasto_chipax_id' => 'id']],
